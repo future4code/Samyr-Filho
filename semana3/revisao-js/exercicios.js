47e5133bb7db3c893571de7878e13901f63c103d
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-06-29 18:50:24 -0300

    Finalização dos exercícios do dia 28/06/2021 - Revisão JS

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -190,72 +190,150 @@ function ordenaArray(array) {
 function filmeFavorito() {
 const filme = {
   nome: 'O Diabo Veste Prada',
-  
-
-}
+  ano: 2006,
+  diretor: 'David Frankel',
+  atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+  }
+  return filme
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  return `Venha assistir ao filme ${filmeFavorito().nome}, de ${filmeFavorito().ano}, dirigido por ${filmeFavorito().diretor} e estrelado por ${filmeFavorito().atores[0]}, ${filmeFavorito().atores[1]}, ${filmeFavorito().atores[2]}, ${filmeFavorito().atores[3]}.`
 }
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
-
+const infoRetangulo = {
+  largura: lado1,
+  altura: lado2,
+  perimetro: 2 * (lado1 + lado2),
+  area: lado1 * lado2
+}
+return infoRetangulo
 }
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
-
+let pessoaAnonima = {...pessoa}
+pessoaAnonima.nome = 'ANÔNIMO'
+return pessoaAnonima
 }
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
-
+const adultos = arrayDePessoas.filter((pessoa) => {
+  return pessoa.idade >= 18
+}) 
+return adultos
 }
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
-
+  const criancas = arrayDePessoas.filter((pessoa) => {
+    return pessoa.idade < 18
+  }) 
+  return criancas
 }
 
 // EXERCÍCIO 17A
 function multiplicaArrayPor2(array) {
-
+  const dobroDosNumeros = array.map((numero) => {
+    return numero * 2
+  }) 
+  return dobroDosNumeros
 }
 
 // EXERCÍCIO 17B
 function multiplicaArrayPor2S(array) {
-
+  const dobroDosNumerosS = array.map((numero) => {
+    let dobro = numero * 2
+    return dobro.toString()
+  }) 
+  return dobroDosNumerosS
 }
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
-
+  const paridadeNumero = array.map((numero) => {
+    let parOuImpar = ''
+    if (numero % 2 === 0){
+      parOuImpar = 'par'
+    } else {
+      parOuImpar = 'ímpar'
+    }
+    return `${numero.toString()} é ${parOuImpar}`
+  }) 
+  return paridadeNumero 
 }
 
 // EXERCÍCIO 18A
 function retornaPessoasAutorizadas(pessoas) {
-
+  const pessoasComPermissao = pessoas.filter((pessoa)=>{
+    return (pessoa.altura >= 1.5)  && (pessoa.idade > 14) && (pessoa.idade < 60)
+  })
+  return pessoasComPermissao
 }
 
 // EXERCÍCIO 18B
 function retornaPessoasNaoAutorizadas(pessoas) {
-
+  const pessoasSemPermissao = pessoas.filter((pessoa)=>{
+    let alturaNaoOK = pessoa.altura < 1.5
+    let menor14 = pessoa.idade <= 14
+    let maior60 = pessoa.idade >= 60
+  return alturaNaoOK || menor14 || maior60
+})
+return pessoasSemPermissao
 }
 
 // EXERCÍCIO 19A
 function ordenaPorNome(consultasNome) {
-
+  let consulta = consultasNome
+  for(let iPrincipal = 0; iPrincipal < consulta.length; iPrincipal++){
+    for (let iSecundario = 0; 
+             iSecundario < consulta.length - iPrincipal - 1;
+             iSecundario++){
+               if(consultasNome[iSecundario].nome > consultasNome[iSecundario + 1].nome){
+                 let temporario = consultasNome[iSecundario]
+                 consultasNome[iSecundario] = consultasNome[iSecundario + 1]
+                 consultasNome[iSecundario + 1] = temporario
+               }
+             }
+  }
+return consulta
 }
 
 // EXERCÍCIO 19B
 function ordenaPorData(consultasData) {
+  // Função auxiliar para converter a data para a comparação
+  const dataStringParaDataValue = (data)=>{
+    const dataString = data.split('/')
+    const dataNewDate = new Date(dataString[2], dataString[1] - 1, dataString[0])
+    return dataNewDate.valueOf()
+  }
 
+  let consulta = consultasData
+  for(let iPrincipal = 0; iPrincipal < consulta.length; iPrincipal++){
+    for (let iSecundario = 0; 
+            iSecundario < consulta.length - iPrincipal - 1;
+            iSecundario++){
+              if(dataStringParaDataValue(consulta[iSecundario].dataDaConsulta) > 
+                  dataStringParaDataValue(consulta[iSecundario + 1].dataDaConsulta)){
+                    let temporario = consulta[iSecundario]
+                    consulta[iSecundario] = consulta[iSecundario + 1]
+                    consulta[iSecundario + 1] = temporario       
+              }
+            }
+  }
+  return consulta
 }
 
 // EXERCÍCIO 20
 function calculaSaldo(contas) {
-
+  for (conta of contas){
+    let totalCompras = conta.compras[0] + conta.compras[1] + conta.compras[2]  
+    conta.saldoTotal = conta.saldoTotal - totalCompras
+    }
+  return contas
 }
