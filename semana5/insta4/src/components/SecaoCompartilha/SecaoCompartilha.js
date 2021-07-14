9cd13726683903751e291f361e44e11592d0dd6e
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-07-14 16:52:19 -0300

    Projeto Post do Instagram

---
+++ semana5/insta4/src/components/SecaoCompartilha/SecaoCompartilha.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CompartilhaContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+// const InputComentario = styled.input`
+//     width: 100%;
+//     margin-right: 5px;
+// `
+
+// export class SecaoComentario extends Component {
+// 	state = {
+
+// 	}
+
+// 	onChangeComentario = (event) => {
+// 		console.log(event.target.value)
+// 		this.setState({
+// 			comentarioDigitado: event.target.value
+// 		})
+		
+// 	}
+
+// 	render() {
+// 		return <CommentContainer>
+// 			<InputComentario
+// 				placeholder={'Comentário'}
+// 				value={this.state.comentarioDigitado}
+// 				onChange={this.onChangeComentario}
+// 			/>
+// 			<button onClick={this.props.aoEnviar}>Enviar</button>
+// 		</CommentContainer>
+// 	}
+// }
+
+export class SecaoCompartilha extends Component {
+    render() {
+        return 
+        (
+        <CompartilhaContainer>
+            
+        </CompartilhaContainer>
+        )
+    }
+}
