c9b7f4564530ce2c01968eed48c17a50af165fdf
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-09-20 18:20:16 -0300

    Feito o segundo desafio

--- semana-15/aula-41/src/Exercicio3/Index.js
+++ semana-15/aula-41/src/Exercicio3/Index.js
@@ -1,4 +1,30 @@
-let listaTarefa = ['Ligar para Carla', 'Pagar conta de celular']
+const fs = require('fs');
+let listaTarefa = []
+const atualizaArquvo = () => {
+    let data = JSON.stringify(listaTarefa)
+    try{
+        fs.writeFileSync('./src/public/Lista.json', data)
+        // fs.statSync('./src/public/Lista.json')
+        // fs.unlink("./src/public/Lista.json")
+    }
+    catch (e) {
+        fs.unlink("./src/public/Lista.json")
+        fs.writeFileSync('./src/public/Lista.json', data)
+    }
+    
+}
+const lerArquivo = ()=>{
+    try {
+        fs.statSync('./src/public/Lista.json')
+        const data = fs.readFileSync('./src/public/Lista.json')
+        listaTarefa = JSON.parse(data)
+      }
+      catch (e) {
+        atualizaArquvo()
+      }
+}
 const tarefa = process.argv[2]
+lerArquivo()
 listaTarefa.push(tarefa)
-console.log(listaTarefa)
+atualizaArquvo()
+console.log('lista', listaTarefa)
