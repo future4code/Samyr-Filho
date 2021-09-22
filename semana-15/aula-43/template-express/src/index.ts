50e2ff8421bc15b752c1a29fffb86cc2e7e32956
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-09-22 20:25:42 -0300

    Feito até o Exercício 5

--- semana-15/aula-43/template-express/src/index.ts
+++ semana-15/aula-43/template-express/src/index.ts
@@ -22,35 +22,30 @@ app.get('/countries/search', (req: Request, res: Response) => {
     
     try {
         let result = [...countries]
-        const name = req.query.name;
-        const capital = req.query.capital;
-        const continent = req.query.continent;
 
         if((!req.query.name) && (!req.query.capital) && (!req.query.continent)) {
             res.statusCode = 400
-            throw new Error('Bad Request ' + req.query.name + ' ' + req.query.capital)
+            throw new Error('Bad Request')
         };
 
         if(req.query.name) {
             result = result.filter((country) => {
-                return country.continent.includes(`${name}`)
+                return country.name.includes(`${req.query.name}`)
             })
         };
         if(req.query.capital) {
             result = result.filter((country) => {
-                return country.continent === req.query.capital
+                return country.capital.includes(`${req.query.capital}`)
             })
         };
         
         if(req.query.continent){
             result = result.filter((country) => {
-                return country.continent === req.query.continent
+                return country.continent.includes(`${req.query.continent}`)
             })
         };
 
         res.send(result)
-            // 
-        // && (req.params.capital === '') && (req.params.continent === '')){
     }
     catch (error: any) {
         res.send(error.message)
@@ -70,6 +65,76 @@ app.get('/countries/:id', (req: Request, res: Response) => {
             throw new Error('id not found!')
         };
         res.send(result)
+    }
+    catch (error: any) {
+        res.send(error.message)
+    }
+})
+app.put('/countries/:id', (req: Request, res: Response) => {
+    try {
+        const ehNumber : number = Number(req.params.id) 
+        if((!ehNumber) && (req.params.id !== '0')) {
+            res.statusCode = 400
+            throw new Error('Invalid id!')
+        };
+        
+        if((!req.body.name) && (!req.body.capital)){
+            res.statusCode = 400
+            throw new Error('Bad Request')
+        };
+
+        const index = countries.findIndex((country) => {
+            return country.id === Number(req.params.id)
+        })
+        
+        if(index === -1){
+            res.statusCode = 404
+            throw new Error('id not found!')
+        };
+
+        if(req.body.name){
+            countries[index].name = req.body.name    
+        }
+
+        if(req.body.capital){
+            countries[index].capital = req.body.capital
+        }
+        res.send(countries[index])
+    }
+    catch (error: any) {
+        res.send(error.message)
+    }
+})
+
+app.delete('/countries/:id', (req: Request, res: Response) => {
+    try {
+        const ehNumber : number = Number(req.params.id) 
+        if((!ehNumber) && (req.params.id !== '0')) {
+            res.statusCode = 400
+            throw new Error('Invalid id!')
+        };
+
+        if(!req.headers.authorization){
+            res.statusCode = 404
+            throw new Error('Authorization not found!')
+        }
+
+        const index = countries.findIndex((country) => {
+            return country.id === Number(req.params.id)
+        })
+
+        if(index === -1){
+            res.statusCode = 404
+            throw new Error('id not found!')
+        };
+
+        if(req.headers.authorization !== "hsoeu460173") {
+            res.statusCode = 401
+            throw new Error('Not Authorization!')
+        }
+
+        countries.splice(index, 1)
+        res.status(204).end()
     }
     catch (error: any) {
         res.send(error.message)
