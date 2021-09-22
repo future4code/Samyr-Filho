82636ca12d793ca0006b0b4433b851f906f4a146
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-09-22 14:21:38 -0300

    Complementos

--- semana-15/aula-42/src/Exercicio2/index.ts
+++ semana-15/aula-42/src/Exercicio2/index.ts
@@ -27,7 +27,37 @@ function obterEstatisticas(numeros: number[]) {
 //b)As outras variáveis são o array 'numerosOrdenados' que recebe o array passado
 //  como parametro ordenado, a number 'soma' para acumular os numeros do array 'numeros'
 //  e estatisticas que foi tiptificada como um objeto tipificado.
+
 //c)
+
+type resposta = {
+    maior: number,
+    menor: number,
+    media: number
+}    
+
+const estatisticas1 = (numeros: number[])=>{
+    const numerosOrdenados: number[] = numeros.sort(
+        (a, b) => a - b
+    )
+    let soma: number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas: resposta = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+type amostra = {
+    numeros: number[],
+    estatistica: () => {estatisticas1(numeros: number[]): resposta}
+}
 
 
 
