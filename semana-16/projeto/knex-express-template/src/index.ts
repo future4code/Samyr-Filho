d996b09718ebc4a3896434b1a3227aba856612d8
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-10-01 18:29:05 -0300

    Fiz até exercício 4

--- semana-16/projeto/knex-express-template/src/index.ts
+++ semana-16/projeto/knex-express-template/src/index.ts
@@ -21,12 +21,12 @@ const server = app.listen(process.env.PORT || 3003, () => {
 });
 
 const getUserById = async (id:number): Promise<any> => {
-    const result = await connection.raw(`
-    SELECT id, nickname FROM Users WHERE id = "${id}"
-    `)
-    
-    return result[0][0]
- }
+    const result = await connection("Users")
+      .where({
+         id: id
+      })
+   return result
+ };
 
  const createUser = async (name: string, nickName: string, email: string): Promise<any> => {
     await connection("Users")
@@ -35,37 +35,57 @@ const getUserById = async (id:number): Promise<any> => {
         nickName,
         email
     });
- }
+ };
+
+const createTask = async (title: string, description: string, 
+                          limitDate: string, creatorUserId: number): Promise<any> => {
+    await connection("Users")
+    .insert({
+        title,
+        description,
+        limitDate,
+        creatorUserId
+    });
+ };
 
  const changeUser = async (id: number, name: string, nickName: string): Promise<any> => {
-    const result = await connection("Users")
-       .update({
-          name: name,
-          nickname: nickName
-       })
-       .where({
-          id: id
-       });
- }
-
+    const findUser = await getUserById(id);
+    if(!findUser){
+        return ''
+    } else {
+        await connection("Users")
+        .update({
+            name: name,
+            nickname: nickName
+        })
+        .where({
+            id: id
+        });
+        return `Update successefully`
+    };
+    
+ };
 
 app.get("/user/:id", async (req, res) => {
     let errorCode: number = 400;
     try {
         const id: number = Number(req.params.id);
-        // let result = {};
-        console.log(id)
         if (isNaN(id)) {
             errorCode = 422; //unprocessable entity
-            throw new Error("Invalid value for id. Please send a number.");
+            
         }
-        const result = getUserById(id);
+        const result = await getUserById(id);
+        if(result.length === 0){
+            errorCode = 404;
+            throw new Error("id not found.");
+        }
         res.send(result).end()
     }
     catch (error: any) {
         res.status(errorCode).send({ message: error.message });
-    }
-    
+    };
+});    
+
 app.post("/user", async (req, res) => {
         let errorCode: number = 400;
         try {
@@ -75,21 +95,17 @@ app.post("/user", async (req, res) => {
                 throw new Error("Please check the fields!");
               }
             await createUser(name, nickname, email);
-            res.end()
+            res.status(201).send({ message: "User created successefully" });
         }
         catch (error: any) {
             res.status(errorCode).send({ message: error.message });
         }
-    })    
-    
 });
 
 app.put("/user/edit/:id", async (req, res) => {
     let errorCode: number = 400;
     try {
-        const id: number = Number(req.params.id);
-        let result = {};
-        console.log(id)
+        const id: number = Number(req.params.id);console.log(id)
         if (isNaN(id)) {
             errorCode = 422; //unprocessable entity
             throw new Error("Invalid value for id. Please send a number.");
@@ -99,8 +115,29 @@ app.put("/user/edit/:id", async (req, res) => {
             errorCode = 422;
             throw new Error("Please check the fields!");
         }
-        result = changeUser(id, name, nickname);
-        res.status(200).end(result)
+        const result = await changeUser(id, name, nickname);
+        if(result === ''){
+            errorCode = 404;
+            throw new Error("Id not found!");
+        }
+        res.status(200).send(result).end()
+    }
+    catch (error: any) {
+        res.status(errorCode).send({ message: error.message });
+    };
+});
+
+app.post("/task", async (req, res) => {
+    let errorCode: number = 400;
+    try {
+        const {title, description, limitDate, creatorUserId} = req.body;
+        if (!title || !description || !limitDate || creatorUserId) {
+            errorCode = 422;
+            throw new Error("Please check the fields!");
+          }
+        
+        await createTask(title, description, limitDate, creatorUserId);
+        res.status(201).send({ message: "User created successefully" });
     }
     catch (error: any) {
         res.status(errorCode).send({ message: error.message });
