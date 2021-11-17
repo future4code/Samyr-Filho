7c4ec594d475adb752356659c917fed339ae6730
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2021-11-17 15:56:49 -0300

    acerto user

--- semana19/cookenu/src/model/users/user.ts
+++ semana19/cookenu/src/model/users/user.ts
@@ -1,14 +1,10 @@
-export enum USER_ROLES {
-    NORMAL = "NORMAL",
-    ADMIN = "ADMIN"
-}
-
 export class User {
     constructor(
         private id: string,
         private name: string,
-        private email: String,
-        private password : string
+        private email: string,
+        private password : string,
+        private role: USER_ROLES
     ) {
 
     }
