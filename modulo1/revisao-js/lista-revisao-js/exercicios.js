430731903c6a598a0e00bfa424c20f4217901722
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2022-02-01 15:31:26 -0300

    Exercício 6

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -41,7 +41,8 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    const novoArray = array.sort((a,b)=>(a-b))
+    return novoArray[novoArray.length - 1]
 }
 
 // EXERCÍCIO 07
