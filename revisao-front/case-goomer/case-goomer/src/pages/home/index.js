6a62914baff0c6fccfafddc4357c02a24a7430ef
Samyr Chiade Hissa Filho <samyr@schissa.com.br>
2022-02-18 17:34:10 -0300

    Populando e reinderizando o restaurantCard

--- revisao-front/case-goomer/case-goomer/src/pages/home/index.js
+++ revisao-front/case-goomer/case-goomer/src/pages/home/index.js
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react"
+import { RestaurantCard } from "../../components";
 import { useRequestData } from "../../hooks/useRequestData";
 import { getRestaurants } from "../../utils/api";
 
 export const HomePage = () => {
     const [restaurants, setRestaurants] = useState([]);
     useEffect(()=>{
-        // const fetch = async () => { 
-        //     const {response, error} = await getRestaurants();
-        //     if(!error){
-        //         setRestaurants(response)
-        //     } else {
-        //         console.log('error', error);
-        //     }
-        // }
-        // fetch()
-        const {data} = getRestaurants()
-        console.log('data', data);
+        const fetch = async () => { 
+            const {result, error} = await getRestaurants();
+            if(!error){
+                setRestaurants(result)
+            } else {
+                console.log('error', error);
+            }
+        }
+        fetch()
     }, [])
+
     return (
-        <h1>HomePage</h1>
+        <>
+            <h1>HomePage</h1>
+            {restaurants.map((restaurant)=>(
+                <RestaurantCard key={restaurant.id} 
+                                name={restaurant.name}
+                                address={restaurant.address} />
+                                ))
+            }
+    
+        
+        </>
+        
     )
 }
--- revisao-front/case-goomer/case-goomer/src/utils/api.js
+++ revisao-front/case-goomer/case-goomer/src/utils/api.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 import { useRequestData } from '../hooks/useRequestData';
 
 const axiosGet = async (url) => {
-    let response;
+    let result;
     let error;
     try {
         const { data } = await axios.get(url);
-        response = data;
+        result = data;
     } catch(e) {
         error = e;
     }
     return {
-        response,
+        result,
         error
     }
 }
 
-export const getRestaurants = () => axiosGet('http://challange.goomer.com.br/restaurants');
+export const getRestaurants =  () => axiosGet('http://challange.goomer.com.br/restaurants');
 export const getMenu = (id) => axiosGet(`http://challange.goomer.com.br/restaurants/${id}/menu`);
